db41f29e3e79ba6d7daf5b74f82bf4e03e94b08d
Alvaro Marenco <alvaromarencom@gmail.com>
2025-04-28 22:07:36 -0600

    CalendarSlice

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,29 +1,19 @@
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { addHours } from 'date-fns'
 import { Navbar, CalendarEvent, CalendarModal} from "../"
 import { localizer, getMessagesES } from '../../helpers';
 import { useState } from 'react';
-import { useUiStore } from '../../hooks';
+import { useUiStore , useCalendarStore} from '../../hooks';
 
 
 
-const events = [{
-  title: 'Cumpleaños del jefe',
-  notes: 'Hay que comprar el pastel',
-  start: new Date(),
-  end: addHours(new Date(), 2),
-  bgColor: '#fafafa',
-  user: {
-    _id: '123',
-    name: 'Alvaro'
-  }
-}]
 
 export const CalendarPage = () => {
 
   const {openDateModal} = useUiStore();
+
+  const {events} = useCalendarStore();
 
   const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
 
