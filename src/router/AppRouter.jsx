8eafad3b4ae054c77ab6e05676becfbfe525da75
Alvaro Marenco <alvaromarencom@gmail.com>
2025-05-22 14:11:08 -0600

    Changing URL after authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -15,7 +15,7 @@ export const AppRouter = () => {
 
   useEffect(() => {
     checkAuthToken()
-  }, );
+  }, []);
 
   if(status === 'checking') {
     return (
@@ -28,11 +28,20 @@ export const AppRouter = () => {
     <Routes>
         {
             (status === 'not-authenticated')
-            ? <Route path="/auth/*" element={<LoginPage/>} />
-            : <Route path="/*" element={<CalendarPage/>} />
+            ? (
+              <>
+                <Route path="/auth/*" element={<LoginPage/>} />
+                <Route path="/*" element={<Navigate to="/auth/login" />} />
+              </>
+              )
+            : (
+              <>
+                <Route path="/" element={<CalendarPage/>} />
+                <Route path="/*" element={<Navigate to="/" />} />
+              </>
+              )
         }
 
-        <Route path="/*" element={<Navigate to="/auth/login" />} />
         
     </Routes>
   )
